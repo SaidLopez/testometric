a34f2e55815a44405ea3838de1a08e97ac27ee98
Said Lopez <SLopez@flexitallic.eu>
2021-11-19 13:40:39 +0000

    all done

---
+++ test.py
@@ -0,0 +1,75 @@
+import PySimpleGUI as sg
+import pandas as pd
+import os
+
+def read_files(files, thicknesses):
+    
+    dfs = []
+    
+    for i,file in enumerate(files):
+        data = pd.read_csv(file, sep = ',', header = None )
+        data.columns = [f'Force (N) {i}',f'Deflection (mm) {i}',f'Time (s) {i}']
+        data[f'Stress (MPA) {i}'] = data[f'Force (N) {i}'] / 550
+        data[f'Thinkess (mm) {i}'] = thicknesses[i] - data[f'Deflection (mm) {i}']
+        dfs.append(data)
+    
+    df = pd.concat(dfs, axis = 1)
+    return df.to_csv('Wrapped data.csv')
+
+sg.theme('Dark Blue 3')
+
+
+### This is the original Layout to access everything in the tool ###
+
+layout1 = [
+    [sg.Text('Select a Directory (Only txt files outputted from the Testometric Kit)')],
+    [sg.Input(size =(15,1)),sg.FolderBrowse(key="_FOLDER_")],
+    [sg.OK(),sg.Cancel()]
+]
+
+
+layout2 = [
+    [sg.Text('Input Gasket thicknesses',justification ='center', size=(50,1), font=['Comic Sans',15])],
+    [sg.Frame('',[[sg.T('Thicknesses')]], key='-COL1-')],
+    [sg.B("Create File", key = '-CSVFILE-')],
+    [sg.Text('All done - Check the folder to see the final csv',key='_RESULT_', visible=False)]
+    #Reused in the Google updates layout
+    #[sg.Button(button_text="Back")]
+]
+
+
+
+
+layout = [
+    [sg.Column(layout1, key='-MAIN-',element_justification = 'c'), 
+     sg.Column(layout2, visible=False, key='_GASKETS_',element_justification = 'c')
+    ]
+    
+]
+# Create the window
+window = sg.Window("Testometric data wrapper", layout, size = (800,500), element_justification = 'c')
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "Cancel" or event == sg.WIN_CLOSED:
+        break
+    elif event == 'OK':
+        files_list = os.listdir(values['_FOLDER_'])
+        window['-MAIN-'].update(visible=False)
+        layout = '_GASKETS_'
+        window[layout].update(visible=True)
+        for i in range(len(files_list)-1):
+            window.extend_layout(window['-COL1-'], [[sg.T(f'Gasket Thickness {i}'), sg.I(key=f'-IN-{i}-')]])
+        
+        
+    elif event == '-CSVFILE-':
+        gasket_thicknesses = [int(values[f'-IN-{i}-']) for i in range(len(files_list)-1) ]
+        
+        read_files(files_list,gasket_thicknesses)
+        window.Element('_RESULT_').update(visible = True)
+    
+           
+window.close()
